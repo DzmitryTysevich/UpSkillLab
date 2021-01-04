49a5253fa7df19b7a2fc6db6da1067143b184fe7
DzmitryTysevich <dztysevich@gmail.com>
2021-01-04 13:53:24 +0300

    Module git: task

--- src/com/epam/task/git/MergeRequest.java
+++ src/com/epam/task/git/MergeRequest.java
@@ -2,6 +2,6 @@ package com.epam.task.git;
 
 public class MergeRequest {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Nice to meet you");
     }
 }
